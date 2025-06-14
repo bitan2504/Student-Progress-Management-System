186e09231ad5d7658a22a95f8bf5e419066b7beb
bitan2504 <biggyansagar@gmail.com>
2025-06-14 14:10:01 +0530

    feat: implement Admin model with authentication and token management

---
+++ server/src/models/admin.js
@@ -0,0 +1,61 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+/**
+ * Mongoose schema for the Admin model.
+ *
+ * Represents an administrator with authentication credentials.
+ *
+ * @typedef {Object} Admin
+ * @property {string} username - Unique username for the admin. Required.
+ * @property {string} password - Hashed password for the admin. Required.
+ * @property {string} [refreshToken] - Optional refresh token for session management.
+ */
+const adminSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    refreshToken: {
+        type: String,
+    },
+});
+
+adminSchema.pre('save', function (next) {
+    if (this.isModified === false) {
+        this.password = bcrypt.hashSync(this.password, 10);
+    }
+    next();
+});
+
+adminSchema.methods.comparePassword = function (token) {
+    return bcrypt.compareSync(token, this.password);
+};
+
+adminSchema.methods.createAccessToken = function () {
+    return jwt.sign(this._id, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    });
+};
+
+adminSchema.methods.createRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            username: this.username,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+        }
+    );
+};
+
+export default mongoose.model('Admin', adminSchema);
